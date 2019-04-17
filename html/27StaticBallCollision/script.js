12fb13c608dd2e4ec7584b75d1ddecb2f98ea83a
PeterSchreuder_Laptop <24441@ma-web.nl>
2019-04-17 15:13:25 +0200

    push

--- html/27StaticBallCollision/script.js
+++ html/27StaticBallCollision/script.js
@@ -41,6 +41,7 @@ function Update()
     A.point.position(A.pos);
     B.point.position(B.pos);
 
+    WallCollision(A)
     PlayerCollision(A, B);
 
     B.point.draw(context, "Punt: B", true);
@@ -69,21 +70,50 @@ function PlayerCollision(_point, _other)
     _point.rad.differenceVector(_other.pos, _point.pos);
 
     let _dis_between_mag = _point.rad.magnitude;
-    console.log(_dis_between_mag)
+
+    if (_dis_between_mag <= _pr + _por)
+    {
+        //_point.pos.substract(_point.vel);
+        //Normalize them
+        _point.rad.magnitude = 1;
+        _point.tan.magnitude = 1;
+
+        //Dot product
+        _point.rad.magnitude = _point.vel.Dot(_point.rad);
+        _point.tan.magnitude = _point.vel.Dot(_point.tan);
+
+        //Miror the direction
+        _point.rad.angle += Math.PI;
+        _point.vel.SumVector(_point.rad, _point.tan);
+
+        /*
+        _point.vel.dx = -_point.vel.dx;
+        _point.vel.dy = -_point.vel.dy;
+         */
+
+        if (_point.point.pointColor != "green")
+            _point.point.pointColor = "green";
+        else
+            _point.point.pointColor = "red";
+    }
+}
+
+function WallCollision(A)
+{
+    var _pr = A.point.r;
 
     //- Horizontal
-    if (_point.pos.dx < _pr || _point.pos.dx > canvas_width - _pr)
-        _point.vel.dx = -_point.vel.dx;
+    if (A.pos.dx < _pr || A.pos.dx > canvas_width - _pr)
+        A.vel.dx = -A.vel.dx;
 
     //- Vertical
-    if (_point.pos.dy < _pr || _point.pos.dy > canvas_height - _pr)
-        _point.vel.dy = -_point.vel.dy;
+    if (A.pos.dy < _pr || A.pos.dy > canvas_height - _pr)
+        A.vel.dy = -A.vel.dy;
+}
 
 
-    if (_dis_between_mag <= _por + _pr)
-    {
-
-        _point.rad.r = 1;
+/*
+_point.rad.r = 1;
 
         _point.tan.dx = -_point.rad.dy;
         _point.tan.dy = _point.rad.dx;
@@ -98,16 +128,4 @@ function PlayerCollision(_point, _other)
 
         //Tel tangieel & radieel bij elkaar op voor nieuwe snelheid
         _point.vel.SumVector(_point.rad, _point.tan);
-
-
-        /*
-        _point.vel.dx = -_point.vel.dx;
-        _point.vel.dy = -_point.vel.dy;
-         */
-
-        if (_point.point.pointColor != "green")
-            _point.point.pointColor = "green";
-        else
-            _point.point.pointColor = "red";
-    }
-}
+ */
